2b0f9856613c13aa7ce31fc60689af677dc574fa
Zohaib <zohaibmanzoor1700@gmail.com>
2022-03-01 19:40:27 -0800

    fetchBySearch

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-const btn = document.getElementById('push-button')
+    const btn = document.getElementById('push-button')
+    const userInput = document.getElementById('user-input').value 
 
-btn.addEventListener("click", (e) => {
-    e.preventDefault()
-    fetch('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then(data => console.log(data))
+    btn.addEventListener("click", (e) => {
+        e.preventDefault()
+        let userChoice = userInput
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userChoice}`)
+            .then(res => res.json())
+            .then(data => {
+                data.drinks.forEach(drink => {
+                    console.log(drink.strDrink)
+                });
+            })
+    })
 })
 
-})
+// http://www.thecocktaildb.com/api/json/v1/1/random.php
